91bde1d944da5454db8e80db375f22dd49897766
Bohdan <bohdansharipov@gmail.com>
2018-05-27 17:19:46 -0400

    Enabled Spring boot app for the project. #1

---
+++ src/main/java/com/trio/budgt/BudgtApplication.java
@@ -0,0 +1,12 @@
+package com.trio.budgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgtApplication.class, args);
+	}
+}
