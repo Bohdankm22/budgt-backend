9b4a340df1247131c4555e5bd53f67e20dbf6bf2
Bohdan <bohdansharipov@gmail.com>
2018-05-28 18:34:10 -0400

    Added fixes in names and exception message. #13 (#14)

--- src/main/java/com/trio/budgt/exception/UserNotFoundException.java
+++ src/main/java/com/trio/budgt/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.trio.budgt.exception;
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(Long userId) {
-        super(String.format("Could not user with id %d", userId));
+        super(String.format("Could not find user with id %d", userId));
     }
 
 }
