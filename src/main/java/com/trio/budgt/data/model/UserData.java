7a84155cd552c48a71f2f4ae9182cd4005d17ec9
Bohdan <bohdansharipov@gmail.com>
2018-05-30 19:27:18 -0400

    Added UserData model. #15 (#16)

---
+++ src/main/java/com/trio/budgt/data/model/UserData.java
@@ -0,0 +1,58 @@
+package com.trio.budgt.data.model;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "USERS_DATA")
+@EntityListeners(AuditingEntityListener.class)
+public class UserData implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Column(nullable = false, updatable = false)
+    private String firstName;
+
+    @Column(nullable = false, updatable = false)
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
