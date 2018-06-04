6bf815be26b3dce4004a3e55f23431d4acdee02e
Bohdan <bohdansharipov@gmail.com>
2018-06-04 13:42:43 -0400

    Added logging to the controllers #20 (#22)

--- src/main/java/com/trio/budgt/controller/UserController.java
+++ src/main/java/com/trio/budgt/controller/UserController.java
@@ -3,6 +3,8 @@ package com.trio.budgt.controller;
 import com.trio.budgt.data.model.User;
 import com.trio.budgt.data.repository.UserRepository;
 import com.trio.budgt.exception.UserNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,8 +14,13 @@ import javax.validation.Valid;
 @RequestMapping("/api/v1")
 public class UserController {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final UserRepository userRepository;
+
     @Autowired
-    UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     // Create a new User
     @PostMapping("/users")
@@ -24,6 +31,7 @@ public class UserController {
     // Get a Single User
     @GetMapping("/users/{id}")
     public User getUserById(@PathVariable(value = "id") Long userId) {
+        log.info(String.format("Getting user with id %d", userId));
         return userRepository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException(userId));
     }
